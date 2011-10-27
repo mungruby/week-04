e342351c1522a257213d44359b251b6605e381c4
R. Scott Shaffer <r_scott.shaffer@comcast.net>
2011-10-26 18:43:51 -0700

    examples pass for search_for_quote

--- lib/search_for_quote.rb
+++ lib/search_for_quote.rb
@@ -7,3 +7,21 @@
 # @see http://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Method_Calls
 # @see Programing Ruby, Chapter 6 
 #
+def search_for_quote a_hash
+  @quotes = all_quotes a_hash.delete(:file)
+  return @quotes if @quotes.empty? or a_hash.empty?
+
+  results = []  
+  a_hash.each do |criterion|
+    results.concat @quotes.select {|quote| quote =~ create_regexp(*criterion)}    
+  end
+  results
+end
+
+def create_regexp criterion, text
+  expr = case criterion
+    when :start_with then Regexp.new "^#{text}"
+    when :end_with   then Regexp.new "#{text}$"
+    when :include    then Regexp.new "#{text}"
+  end
+end
--- spec/search_for_quote_spec.rb
+++ spec/search_for_quote_spec.rb
@@ -2,64 +2,46 @@ require "spec_helper"
 
 describe "#search_for_quote" do
   
-  context "when the file does not exist" do
-    
+  context "when the file does not exist" do    
     it "should return an empty list of results" do
-      pending
-      search_for_quote(:file => "FILE_DOES_NOT_EXIST",:starts_with => "The").should be_empty
+      search_for_quote(:file => "FILE_DOES_NOT_EXIST", :start_with => "The").should be_empty
     end
-    
   end
   
   context "when the file does exist" do
     
     context "when no search criteria has been specified" do
-      
       it "should return all quotes" do
-        pending
         search_for_quote(:file => TEXT_FILE).should_not be_empty
       end
-      
     end
     
     context "when search criteria has been specified" do
-      
-      describe "contains" do
-        
+      describe "include" do
         it "should return all the quotes that contain the criteria" do
-          pending
-          results = search_for_quote(:file => TEXT_FILE,:include => "dude")
+          results = search_for_quote(:file => TEXT_FILE, :include => "dude")
           results.should have(3).items
         end
-        
       end
       
-      describe "starts_with" do
-        
+      describe "start_with" do
         it "should return all the quotes that contain the criteria" do
-          pending
-          results = search_for_quote(:file => TEXT_FILE,:start_with => "Her")
+          results = search_for_quote(:file => TEXT_FILE, :start_with => "Her")
 
           results.should have(1).items
           results.first.should == "Her life is in your hands dude"
         end
-        
       end
       
-      describe "ends_with" do
-        
+      describe "end_with" do
         it "should return all the quotes that contain the criteria" do
-          pending
-          results = search_for_quote(:file => TEXT_FILE,:end_with => "dude")
+          results = search_for_quote(:file => TEXT_FILE, :end_with => "dude")
 
           results.should have(2).items
           results.should == [ 'Calmer than you are dude', 'Her life is in your hands dude' ]
           
-        end
-        
+        end 
       end
     end
-    
-  end
-  
+  end  
 end
