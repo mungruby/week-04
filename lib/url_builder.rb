b37b005adaeda041c3f4a7792e91bbfaa0eb563a
R. Scott Shaffer <r_scott.shaffer@comcast.net>
2011-10-29 09:26:20 -0700

    url_builder passing tests

--- lib/url_builder.rb
+++ lib/url_builder.rb
@@ -1,5 +1,3 @@
-
-
 # 
 # Here is where you will write the #url_builder method that are defined in the 
 # associated specifications file.
@@ -10,3 +8,10 @@
 # @see http://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Method_Calls
 # @see Programing Ruby, Chapter 6 
 #
+def url_builder url, parms = {}
+  return "#{url}?results=10" if parms.empty?
+
+  url << '?'
+  parms.each {|k, v| url << "#{k}=#{v}&" if v}
+  url.chop
+end
--- spec/url_builder_spec.rb
+++ spec/url_builder_spec.rb
@@ -2,46 +2,43 @@ require "spec_helper"
 
 describe "#url_builder" do
   
-  let(:url) { "http://google.com" }
-  
+  # let(:url) {'http://google.com'}
+  def url
+    "http://google.com"
+  end
+
   context "when given no parameters should default to a query with 10 results" do
-    
     it "should call the basic url" do
-      pending
       url_builder(url).should == "#{url}?results=10"
     end
-    
   end
   
   context "when given a single parameter" do
-    
     it "appends the parameters to the url" do
-      pending
-      url_builder(url,:results => 20).should == "#{url}?results=20"
+      url_builder(url, :results => 20).should == "#{url}?results=20"
+      # url_builder(URL, :results => 20).should == "#{URL}?results=20"
+      # url_builder('http://google.com', :results => 20).should == "#{url}?results=20"
+      # url_builder(url, :results => 20).should == "http://google.com?results=20"
     end
-    
   end
   
-  context "when given multiple parametesr" do
-    
+  context "when given multiple parameters" do
     it "should append each parameter with an &" do
-      pending
-      url_builder(url,:results => '20',:search => 'burgers').should == "#{url}?results=20&search=burgers"
+      url_builder(url, :results => '20', :search => 'burgers').should == "#{url}?results=20&search=burgers"
+      # url_builder('http://google.com', :results => '20', :search => 'burgers').should == "#{url}?results=20&search=burgers"
+      # url_builder(url, :results => '20', :search => 'burgers').should == "http://google.com?results=20&search=burgers"
     end
-    
   end
   
   context "when a parameter is nil" do
-    
     it "should ignore the parameter" do
-      pending
-      url_builder(url,:results => '20',:search => 'burgers',:sort_by => nil).should == "#{url}?results=20&search=burgers"
+      url_builder(url, :results => '20', :search => 'burgers', :sort_by => nil).should == "#{url}?results=20&search=burgers"
+      # url_builder('http://google.com', :results => '20', :search => 'burgers', :sort_by => nil).should == "#{url}?results=20&search=burgers"
+      # url_builder(url, :results => '20', :search => 'burgers', :sort_by => nil).should == "http://google.com?results=20&search=burgers"
     end
-    
   end
   
   it "should fail" do
-    pending
     fail
   end
   
